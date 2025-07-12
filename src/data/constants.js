eb38d61e051d4bdc483c7a7969b22c56c8010f46
ALEX SHRESTHA <080bct012.alex@pcampus.edu.np>
2025-07-12 13:23:48 +0545

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,18 +1,15 @@
 export const Bio = {
   name: "Alex Shrestha",
   roles: [
-    "Full Stack Web  Developer",
-       "App Developer",
+    "Full Stack Web Developer",
+    "App Developer",
     "ML enthusiast",
     "Programmer",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/080bct12alex",
   resume: process.env.PUBLIC_URL + "/resume.pdf",
-
-    
-
   linkedin: "https://www.linkedin.com/in/alexstha310/",
   twitter: "https://x.com/alexstha103",
   insta: "https://www.instagram.com/alex_stha310/",
@@ -28,16 +25,11 @@ export const skills = [
         image:
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
-      
       {
         name: "Next Js",
         image:
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
       },
-     
-     
-     
-      
     ],
   },
   {
@@ -52,23 +44,18 @@ export const skills = [
         image:
           "https://w7.pngwing.com/pngs/846/87/png-transparent-mean-solution-stack-express-js-node-js-javascript-github-text-trademark-logo-thumbnail.png",
       },
-      
-     
       {
         name: "MongoDB",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
       },
-       {
+      {
         name: "PostgreSQL",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/800px-Postgresql_elephant.svg.png",
       },
-      
-      
     ],
   },
-  
   {
     title: "Others",
     skills: [
@@ -77,55 +64,38 @@ export const skills = [
         image:
           "https://avatars.githubusercontent.com/u/18133?s=280&v=4",
       },
-      
       {
         name: "Git Hub",
         image:
           "https://avatars.slack-edge.com/2020-11-25/1527503386626_319578f21381f9641cd8_512.png",
       },
-      
       {
         name: "Postman",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7112eGT4aGSghvAHq7i6rmgPOfsSowHFe3A&s",
       },
-      
       {
         name: "Figma",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS62LVhts8-ERiNmlrfHQShVXasUYH38KKSRw&s",
       },
-    
-       {
+      {
         name: "Vercel",
         image:
           "https://assets.vercel.com/image/upload/front/vercel/twitter-card.png",
       },
-
-       {
+      {
         name: "Render",
         image:
           "https://cdn.sanity.io/images/34ent8ly/production/ec37a3660704e1fa2b4246c9a01ab34e145194ad-824x824.png",
       },
-      
-
-      
-      
-       
-
-        {
+      {
         name: "Socket.IO",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/9/96/Socket-io.svg",
       },
- 
-    
-    
-    
-    
     ],
   },
-
   {
     title: "Android",
     skills: [
@@ -139,11 +109,8 @@ export const skills = [
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr5G_XWqJuICwBxkr6DHS4CM1FR00epymzWw&s",
       },
-      
-      
     ],
   },
-
   {
     title: "Machine Learning",
     skills: [
@@ -152,34 +119,27 @@ export const skills = [
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
       },
-     
-      
-   
       {
         name: "scikit-learn",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
       },
-
-       {
+      {
         name: "Pandas",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCpCB6Du8H6Lrm5WIbDcdW59uqoSiL-eeTlw&s",
       },
-      
-        {
+      {
         name: "Numpy",
         image:
           "https://w7.pngwing.com/pngs/811/893/png-transparent-numpy-hd-logo-thumbnail.png",
       },
-       
-     
-        {
+      {
         name: "Matplotlib",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Matplotlib_icon.svg/1024px-Matplotlib_icon.svg.png",
       },
-       {
+      {
         name: "Seaborn",
         image:
           "https://avatars.githubusercontent.com/u/22799945?s=280&v=4",
@@ -187,12 +147,8 @@ export const skills = [
     ],
   },
 ];
-   
 
-export const experiences = [
-];
-  
-
+export const experiences = [];
 
 export const education = [
   {
@@ -201,7 +157,7 @@ export const education = [
     school: "IOE Pulchowk Campus",
     date: "2024 - present",
     desc: "I am currently pursuing a Bachelor's degree in Computer Engineering at IOE Pulchowk Campus.",
-    degree: "Bachelor of Engineering - B.E, Computer  Engineering",
+    degree: "Bachelor of Engineering - B.E, Computer Engineering",
   },
   {
     id: 1,
@@ -211,159 +167,110 @@ export const education = [
     desc: "I completed my class 12 high school education at CCRC, where I studied Science with Computer Science.",
     degree: "NEB(XII), Science with Computer",
   },
-  
 ];
 
 export const projects = [
-
   {
     id: 0,
     title: "Swiftly",
-    description:
-      "A web application designed for E-Commerce ",
-    image:
-      "https://i.ibb.co/Z1X8trVr/swiftly.jpg",
-    tags: [
-       "MERN" ,  "MongoDB" ,"Redis" ,  "Stripe" , " Robust" , "JWT"
-    ],
-    category: "web app",
+    description: "A web application designed for E-Commerce",
+    image: "https://i.ibb.co/Z1X8trVr/swiftly.jpg",
+    tags: ["MERN", "MongoDB", "Redis", "Stripe", "Robust", "JWT"],
+    category: ["web app"],
     github: "https://github.com/080bct12alex/swiftly-click.shop.smile",
     webapp: "https://swiftly-shop.onrender.com",
   },
   {
     id: 1,
     title: "Bondr",
-    description:
-      "A web application designed for real time chat",
-    image:
-      "https://i.ibb.co/ZqTwcjk/bondr.jpg",
-    tags: [
-       "MERN" ,  "Socket.io" ,"Zustand" ,  "React context " , "  JWT" , "TailwindCSS "
-    ],
-    category: "web app",
+    description: "A web application designed for real time chat",
+    image: "https://i.ibb.co/ZqTwcjk/bondr.jpg",
+    tags: ["MERN", "Socket.io", "Zustand", "React context", "JWT", "TailwindCSS"],
+    category: ["web app"],
     github: "https://github.com/080bct12alex/Bondr-Swipe.Chat.Bond",
     webapp: "https://bondr.onrender.com",
   },
-
-   {
+  {
     id: 2,
-    title: "Mend ",
-    description:
-      "A modern social media application powered by Next.js",
-    image:
-      "https://i.ibb.co/FL0S9YgJ/mend.jpg",
-    tags: [
-       "MERN" ,"Next.js","PostgreSQL" ,"TypeScript" , "TailwindCSS "
-    ],
-    category: "web app",
+    title: "Mend",
+    description: "A modern social media application powered by Next.js",
+    image: "https://i.ibb.co/FL0S9YgJ/mend.jpg",
+    tags: ["MERN", "Next.js", "PostgreSQL", "TypeScript", "TailwindCSS"],
+    category: ["web app"],
     github: "https://github.com/080bct12alex/mend-Heal-together-grow-together",
     webapp: "https://mend-one.vercel.app",
   },
-
-
-
-   
-
   {
     id: 3,
     title: "Mend",
-    description:
-      "A Social media APP to reconnect and grow.",
-    image:
-      "https://i.ibb.co/GQtG31JK/4810f570-5b4c-4099-9e7b-5280500bc279.jpg",       
-    tags: [
-       "React Native" ,"EXPO","Supabase" 
-    ],
-    category: "android app",
+    description: "A Social media APP to reconnect and grow.",
+    image: "https://i.ibb.co/GQtG31JK/4810f570-5b4c-4099-9e7b-5280500bc279.jpg",
+    tags: ["React Native", "EXPO", "Supabase"],
+    category: ["android app"],
     github: "https://github.com/080bct12alex/Mend",
-   webapp: "https://www.upload-apk.com/en/d9Pcrmv5txqXwRg",
+    webapp: "https://www.upload-apk.com/en/d9Pcrmv5txqXwRg",
   },
-
   {
     id: 4,
-   title: "NepalEstate",
-
-    description:
-      "A real estate price prediction web app using machine learning, Next.js and Flask",
-    image:
-      "https://i.ibb.co/zTvw3T4W/Capture.png",       
-    tags: [
-       "Machine Learning" ,"Data Science","MlP Regressor" ,"Sckit-learn" 
-    ],
-    category: "machine learning",
+    title: "NepalEstate",
+    description: "A real estate price prediction web app using machine learning, Next.js and Flask",
+    image: "https://i.ibb.co/zTvw3T4W/Capture.png",
+    tags: ["Machine Learning", "Data Science", "MlP Regressor", "Scikit-learn"],
+    category: ["machine learning", "web app"],
     github: "https://github.com/080bct12alex/Nepalestate",
-   webapp: "https://nepalestate.vercel.app",
+    webapp: "https://nepalestate.vercel.app",
   },
-
   {
     id: 5,
-   title: "CineMatch",
-
+    title: "CineMatch",
     description:
-      "A personalized movie recommendation system  that helps users discover new movies based on their preferences.",
-    image:
-      "https://i.ibb.co/Y42Tf3Vp/Capture.png",       
-    tags: [
-       "Machine Learning" ,"Data Science","COLLABORATIVE-filtering" ,"Sckit-learn" 
-    ],
-    category: "machine learning",
+      "A personalized movie recommendation system that helps users discover new movies based on their preferences.",
+    image: "https://i.ibb.co/Y42Tf3Vp/Capture.png",
+    tags: ["Machine Learning", "Data Science", "Collaborative-filtering", "Scikit-learn"],
+    category: ["machine learning"],
     github: "https://github.com/080bct12alex/cinematch",
-   webapp: "https://cinematch3.onrender.com",
+    webapp: "https://cinematch3.onrender.com",
   },
-
-
   {
     id: 6,
-   title: "SplitIQ",
-
-    description:
-      "An webapp for tracking shared expenses.",
-    image:
-      "https://i.ibb.co/214s3Ng1/Screenshot-2025-05-09-185044.png",       
-    tags: [
-       "Next JS" ,"TailwindCSS " ,"Gemini API"
-    ],
-    category: "web app",
+    title: "SplitIQ",
+    description: "A webapp for tracking shared expenses.",
+    image: "https://i.ibb.co/214s3Ng1/Screenshot-2025-05-09-185044.png",
+    tags: ["Next JS", "TailwindCSS", "Gemini API"],
+    category: ["machine learning", "web app"],
     github: "https://github.com/080bct12alex/SplitIQ",
-   webapp: "https://splitiq-beige.vercel.app",
+    webapp: "https://splitiq-beige.vercel.app",
   },
-
-   {
+  {
     id: 7,
-   title: "AlexAI",
-
-    description:
-      "An AI-powered chatbot application.",
-    image:
-      "https://i.ibb.co/hF24KVJT/Screenshot-2025-05-11-163948.png",       
-    tags: [
-       "MERN" ,"TailwindCSS " ,"Gemini API" 
-    ],
-    category: "web app",
+    title: "AlexAI",
+    description: "An AI-powered chatbot application.",
+    image: "https://i.ibb.co/hF24KVJT/Screenshot-2025-05-11-163948.png",
+    tags: ["MERN", "TailwindCSS", "Gemini API"],
+    category: ["machine learning", "web app"],
     github: "https://github.com/080bct12alex/AlexAI",
-   webapp: "https://alexai-ayn3.onrender.com",
+    webapp: "https://alexai-ayn3.onrender.com",
   },
-
-
   {
     id: 8,
-   title: "EasyRyde",
-
-    description:
-      "An app to find your ideal ride effortlessly.",
-    image:
-      "https://i.ibb.co/60n3d9cP/af39b36e-0914-4505-8878-f38b3d1bad3a.jpg",       
-    tags: [
-       "Reactnative" ,"EXPO "  ,"Google map"
-    ],
-    category: "web app",
+    title: "EasyRyde",
+    description: "An app to find your ideal ride effortlessly.",
+    image: "https://i.ibb.co/60n3d9cP/af39b36e-0914-4505-8878-f38b3d1bad3a.jpg",
+    tags: ["React Native", "EXPO", "Google map"],
+    category: ["android app"],
     github: "https://github.com/080bct12alex/EasyRyde",
-   webapp: "https://www.upload-apk.com/en/XFXTAPUgvXDEE0d",
+    webapp: "https://www.upload-apk.com/en/XFXTAPUgvXDEE0d",
+  },
+  {
+    id: 9,
+    title: "Travel Companion AI",
+    description: "A smart travel planner powered by Gemini AI and real-time flight data.",
+    image: "https://i.ibb.co/PZHxcTx9/Capture.png",
+    tags: ["React", "Gemini API", "SerpAPI"],
+    category: ["machine learning", "web app"],
+    github: "https://github.com/080bct12alex/Travel_Companion_AI",
+    webapp: "https://travelcompanionai.onrender.com",
   },
 ];
 
-
-export const TimeLineData = [
-  { year: 2024, text: "Started my journey" },
-  
-];
+export const TimeLineData = [{ year: 2024, text: "Started my journey" }];
