5a87f038e7cb0d82427864cbc0908f43e62c8c59
ALEX SHRESTHA <080bct012.alex@pcampus.edu.np>
2025-09-19 20:14:18 +0545

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -193,7 +193,9 @@ const Contact = () => {
     <Container>
       <Wrapper>
         <Title>Contact</Title>
-        <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+    {/* 
+                 <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+            */}
         
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
