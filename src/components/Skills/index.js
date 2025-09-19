98d23f34f9fb8932d1bad796c01acf4ae4636a68
ALEX SHRESTHA <080bct012.alex@pcampus.edu.np>
2025-09-19 20:15:53 +0545

    Update index.js

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -124,8 +124,10 @@ const Skills = () => {
     <Container id="skills">
       <Wrapper>
         <Title>Skills</Title>
-        <Desc>Here are some of my skills on which I have been working on for the past 1 year.
+      {/*   <Desc>Here are some of my skills on which I have been working on for the past 1 year.
         </Desc>
+            */}
+      
         <SkillsContainer>
           {skills.map((skill) => (
             <Skill>
